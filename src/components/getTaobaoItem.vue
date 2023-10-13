05e9db091706dc7cd26302035f2fa025b5d18df1
Karsten Zhou <1339688323@qq.com>
2023-10-13 12:18:04 +0200

    optimise

--- src/components/getTaobaoItem.vue
+++ src/components/getTaobaoItem.vue
@@ -1,56 +1,46 @@
 <template>
   <div>
-    <n-h2>获取商品数据</n-h2>
-    <n-p>使用下列功能时请务必先手动滑动至页面底部，确保描述图完全加载完毕。</n-p>
-    <n-form label-placement="left" :label-width="80">
-      <n-form-item label="页面名称">
-        <n-input v-model:value="productItem.pagename" />
-      </n-form-item>
-      <n-form-item label="周边价格">
-        <n-input v-model:value="productItem.price" />
-      </n-form-item>
-      <n-form-item label="上架日期">
+    <n-space vertical>
+      <n-alert :show-icon="false" title="获取商品数据">
+        使用下列功能时请务必先手动滑动至页面底部，确保描述图完全加载完毕。
+      </n-alert>
+      <n-input-group>
+        <n-input v-model:value="productItem.pagename" placeholder="page title/product name" />
         <n-date-picker
           v-model:formatted-value="productItem.date"
           value-format="yyyy-MM-dd"
           type="date"
           clearable
-          style="width: 100%"
         />
-      </n-form-item>
-      <n-form-item label="周边主题">
-        <n-input v-model:value="productItem.feat" />
-      </n-form-item>
-      <n-form-item label="下载图片">
-        <n-switch v-model:value="productItem.ifImgDownload" />
-      </n-form-item>
-      <n-form-item label="自动复制">
-        <n-switch v-model:value="productItem.ifAutoCopy" />
-      </n-form-item>
-      <n-form-item>
-        <n-button attr-type="button" @click="getTaobaoItem()" :loading="loading"> 获取 </n-button>
-      </n-form-item>
-    </n-form>
-    <n-code :code="resCode" show-line-numbers />
+        <n-button @click="getTaobaoItem()" :loading="loading"> 获取信息 </n-button>
+      </n-input-group>
+      <n-space justify="space-between">
+        <n-checkbox v-model:checked="ifDownload">下载图片</n-checkbox>
+        <n-checkbox v-model:checked="ifAutoCopy">自动复制</n-checkbox>
+        <n-checkbox v-model:checked="ifGoToWiki">跳转百科</n-checkbox>
+      </n-space>
+      <n-code :code="code" word-wrap style="user-select: all" />
+    </n-space>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import json from '@/json/index.json'
-import { GM_download } from 'vite-plugin-monkey/dist/client'
+import { GM_download, GM_setClipboard } from 'vite-plugin-monkey/dist/client'
 import $ from 'jquery/dist/jquery.slim'
 
-let resCode = ref('')
+let code = ref('')
 let loading = ref(false)
 let productItem = ref({
   pagename: '',
   price: '',
   date: null,
-  feat: '',
-  ifImgDownload: false,
-  ifAutoCopy: false
+  feat: ''
 })
+let ifDownload = ref(false)
+let ifAutoCopy = ref(false)
+let ifGoToWiki = ref(false)
 
 async function getTaobaoItem() {
   productItem.value.pagename = productItem.value.pagename || '页面名称'
@@ -92,7 +82,7 @@ async function getTaobaoItem() {
   //排序和下载图片
   let imgNameList: string[] = [] //图片的文件名列表
   img.forEach((ele, index) => {
-    if (productItem.value.ifImgDownload) {
+    if (ifDownload.value) {
       GM_download(ele, productItem.value.pagename + (index + 1) + ele.slice(-4))
     }
     imgNameList = imgNameList.concat(productItem.value.pagename + (index + 1) + ele.slice(-4))
@@ -116,7 +106,7 @@ async function getTaobaoItem() {
 
   //排序和下载长图
   longImg.forEach((ele, index) => {
-    if (productItem.value.ifImgDownload) {
+    if (ifDownload.value) {
       GM_download(ele, productItem.value.pagename + ' 描述图' + (index + 1) + ele.slice(-4))
     }
     longImgList = longImgList.concat(
@@ -126,7 +116,7 @@ async function getTaobaoItem() {
   longImgStr = longImgList.join('|')
 
   //等待长图加载完毕后输出结果
-  resCode.value = `{{周边信息\n|版权=\n|尺寸=\n|定价=${
+  code.value = `{{周边信息\n|版权=\n|尺寸=\n|定价=${
     productItem.value.price
   }\n|货号=\n|链接（京东）=\n|链接（乐乎市集）=\n|链接（奇货）=\n|链接（淘宝）=${link}\n|链接（天猫）=\n|链接（玩具反斗城）=\n|品牌=${brand}\n|日期=${
     productItem.value.date || ''
@@ -134,10 +124,12 @@ async function getTaobaoItem() {
     productItem.value.feat
   }\n}}\n\n<gallery>\n${imgNameStr}\n</gallery>\n\n{{长图|${longImgStr}}}\n`
 
-  // 如果启用了自动复制，则自动复制结果
-  if (productItem.value.ifAutoCopy) {
-    navigator.clipboard.writeText(resCode.value)
-    alert('复制成功！')
-  }
+  // 如果启用了自动复制，就复制结果
+  ifAutoCopy.value ? GM_setClipboard(code.value, 'text/plain') : null
+
+  // 如果启用了跳转百科，就跳转百科
+  ifGoToWiki.value
+    ? window.open(`//xyy.huijiwiki.com/wiki/${productItem.value.pagename}?action=edit`)
+    : null
 }
 </script>
